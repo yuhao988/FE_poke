b9b46f5c10008ec898854b71fd671f3d609ce599
yuhao988 <huan0268@e.ntu.edu.sg>
2025-06-03 15:48:14 +0800

    Test adding header

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
 function Home() {
   return (
     <div className="App">
       <header className="App-header">
-      <h2>Class List:</h2>
-      <Link to="/classes">Here</Link>
-      <h2>Equipment List:</h2>
-      <Link to="/equipment">Here</Link>
-      <h2>Test battles</h2>
-      <Link to="/battle">Here</Link>
-        
+        <h1>DIY Unit FE Poke</h1>
+        <h2>Class List:</h2>
+        <Link to="/classes">Here</Link>
+        <h2>Equipment List:</h2>
+        <Link to="/equipment">Here</Link>
+        <h2>Test battles</h2>
+        <Link to="/battle">Here</Link>
       </header>
     </div>
   );
